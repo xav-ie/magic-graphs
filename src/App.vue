e1fc616dace2a84a3d5c38b06610973bb1ad8119
yonava <yona@vossandreae.com>
2024-09-30 02:38:03 -0400

    nice looking trace

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import CodeEditor from './CodeEditor.vue';
 </script>
 
 <template>
-  <div class="bg-[#282c34] w-[100vw] h-[100vh] text-white">
+  <div class="bg-[#282c34] w-[100vw] h-[100vh] text-white relative">
     <Graph />
     <CodeEditor />
   </div>
--- src/CodeEditor.vue
+++ src/CodeEditor.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import { useLocalStorage } from '@vueuse/core';
 import { implementations } from './implementations';
 import { Codemirror } from 'vue-codemirror';
@@ -55,15 +55,15 @@ const userFn = useLocalStorage('userFn', 'return graph');
 const userFnError = ref('');
 
 const runner = () => {
-  const fn = new Function(argName,
-    userFn
-      .value
-      .split('\n')
-      .slice(1, -1)
-      .join('\n'))
-  userFnError.value = '';
-  trace.value = [];
   try {
+    const fn = new Function(argName,
+      userFn
+        .value
+        .split('\n')
+        .slice(1, -1)
+        .join('\n'))
+    userFnError.value = '';
+    trace.value = [];
     fn(graphProxy);
   } catch (e) {
     if (e instanceof Error) {
@@ -73,30 +73,42 @@ const runner = () => {
     }
   }
 }
+
+watch(userFn, runner, { immediate: true });
 </script>
 
 <template>
-  <div>
-    <div class="flex gap-3 px-3 py-2 bg-[#282c34]">
-      <button v-for="(val, key) in implementations" :key="key"
-        class="bg-gray-700 px-5 py-1 font-bold text-md rounded-full hover:bg-gray-800"
-        @click="userFn = `${userFuncSig}\n  ${val}\n}`">
-        {{ key }}
-      </button>
-    </div>
-    <h2 class="text-xl">
-      <codemirror
-        v-model="userFn"
-        :tabSize="2"
-        :extensions="[javascript(), oneDark, readOnlyRangesExtension(getReadOnlyRanges)]"
-        :style="{
-          background: 'gray',
-          height: '320px',
-        }"
-      />
-    </h2>
-    <div v-if="userFnError" class="text-red-500 mt-2 text-2xl font-bold">
+  <div class="flex gap-3 px-3 py-2 bg-[#282c34]">
+    <button v-for="(val, key) in implementations" :key="key"
+      class="bg-gray-700 px-5 py-1 font-bold text-md rounded-full hover:bg-gray-800"
+      @click="userFn = `${userFuncSig}\n  ${val}\n}`">
+      {{ key }}
+    </button>
+  </div>
+  <h2 class="text-xl">
+    <codemirror
+      v-model="userFn"
+      :tabSize="2"
+      :extensions="[javascript(), oneDark, readOnlyRangesExtension(getReadOnlyRanges)]"
+      :style="{
+        background: 'gray',
+        height: '50%',
+      }"
+    />
+  </h2>
+  <div class="px-4 py-2 bg-gray-900 pt-8 pb-12">
+    <h1 v-if="userFnError" class="text-red-500 text-2xl font-bold">
       {{ userFnError }}
+    </h1>
+    <div v-else class="text-2xl font-bold flex">
+      Start → &nbsp;
+      <h1 v-for="nodeId in trace" :key="nodeId">
+        <span class="border-4 border-white p-2 rounded-full px-4">
+          {{ nodeId }}
+        </span>
+         → &nbsp;
+      </h1>
+      End
     </div>
   </div>
 </template>
