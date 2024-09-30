e90fdcdfd447a4cdad19353bc7cf7354d8818f43
yonava <yona@vossandreae.com>
2024-09-30 02:57:59 -0400

    cleaned up code editor component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import Graph from './Graph.vue';
 import CodeEditor from './CodeEditor.vue';
+
+const weightedGraph = {
+  1: [{ node: 2, weight: 4 }, { node: 4, weight: 2 }],
+  2: [{ node: 3, weight: 3 }],
+  3: [],
+  4: [{ node: 5, weight: 3 }, { node: 7, weight: 2 }],
+  5: [{ node: 6, weight: 1 }],
+  6: [{ node: 4, weight: 1 }],
+  7: [{ node: 6, weight: 1 }],
+}
+
+const graph = ref<Record<number, number[]>>({
+  1: [2, 4],
+  2: [3],
+  3: [],
+  4: [5, 7],
+  5: [6],
+  6: [4],
+  7: [6],
+})
+
 </script>
 
 <template>
   <div class="bg-[#282c34] w-[100vw] h-[100vh] text-white relative">
-    <Graph />
-    <CodeEditor />
+    <Graph v-model="graph" />
+    <CodeEditor :graph="graph" />
   </div>
 </template>
--- src/CodeEditor.vue
+++ src/CodeEditor.vue
@@ -8,29 +8,13 @@ import { oneDark } from "@codemirror/theme-one-dark";
 import readOnlyRangesExtension from 'codemirror-readonly-ranges';
 import type { EditorState } from '@codemirror/state';
 
-const graph = {
-  1: [2, 4],
-  2: [3],
-  3: [],
-  4: [5, 7],
-  5: [6],
-  6: [4],
-  7: [6],
-}
+const trace = ref<(string | symbol)[]>([]);
 
-const weightedGraph = {
-  1: [{ node: 2, weight: 4 }, { node: 4, weight: 2 }],
-  2: [{ node: 3, weight: 3 }],
-  3: [],
-  4: [{ node: 5, weight: 3 }, { node: 7, weight: 2 }],
-  5: [{ node: 6, weight: 1 }],
-  6: [{ node: 4, weight: 1 }],
-  7: [{ node: 6, weight: 1 }],
-}
+const props = defineProps<{
+  graph: Record<number, number[]>;
+}>();
 
-const trace = ref<any[]>([]);
-
-const graphProxy = new Proxy(graph, {
+const graphProxy = new Proxy(props.graph, {
   get(target, prop, receiver) {
     trace.value.push(prop);
     return Reflect.get(target, prop, receiver);
@@ -78,31 +62,48 @@ watch(userFn, runner, { immediate: true });
 </script>
 
 <template>
+
+  <!-- switch out algorithm -->
   <div class="flex gap-3 px-3 py-2 bg-[#282c34]">
-    <button v-for="(val, key) in implementations" :key="key"
+    <button
+      v-for="(val, key) in implementations"
+      @click="userFn = `${userFuncSig}\n  ${val}\n}`"
+      :key="key"
       class="bg-gray-700 px-5 py-1 font-bold text-md rounded-full hover:bg-gray-800"
-      @click="userFn = `${userFuncSig}\n  ${val}\n}`">
+    >
       {{ key }}
     </button>
   </div>
-  <h2 class="text-xl">
-    <codemirror
-      v-model="userFn"
-      :tabSize="2"
-      :extensions="[javascript(), oneDark, readOnlyRangesExtension(getReadOnlyRanges)]"
-      :style="{
-        background: 'gray',
-        height: '50%',
-      }"
-    />
-  </h2>
-  <div class="px-4 py-2 bg-gray-900 pt-8 pb-12">
-    <h1 v-if="userFnError" class="text-red-500 text-2xl font-bold">
+
+  <!-- code editor -->
+  <codemirror
+    v-model="userFn"
+    :tabSize="2"
+    :extensions="[javascript(), oneDark, readOnlyRangesExtension(getReadOnlyRanges)]"
+    :style="{
+      background: 'gray',
+      height: '320px',
+    }"
+    class="text-lg"
+  />
+
+  <!-- traversal trace output -->
+  <div class="px-6 py-2 bg-gray-900 pt-8 pb-12 rounded-t-2xl">
+    <h1
+      v-if="userFnError"
+      class="text-red-500 text-2xl font-bold"
+    >
       {{ userFnError }}
     </h1>
-    <div v-else class="text-2xl font-bold flex">
+    <div
+      v-else
+      class="text-2xl font-bold flex"
+    >
       Start → &nbsp;
-      <h1 v-for="nodeId in trace" :key="nodeId">
+      <h1
+        v-for="nodeId in trace"
+        :key="nodeId"
+      >
         <span class="border-4 border-white p-2 rounded-full px-4">
           {{ nodeId }}
         </span>
@@ -111,6 +112,5 @@ watch(userFn, runner, { immediate: true });
       End
     </div>
   </div>
-</template>
 
-<style scoped></style>
+</template>
