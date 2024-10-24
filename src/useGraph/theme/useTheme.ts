4476d6fb1533a45f200e6bb5b1e9040b3af78297
yonava <yona@vossandreae.com>
2024-10-24 10:02:45 -0400

    added usetheme scaffold

---
+++ src/useGraph/theme/useTheme.ts
@@ -0,0 +1,28 @@
+import type { Graph } from "../useGraph";
+import type { UnwrapRef } from "vue";
+
+type UseGraphTheme = UnwrapRef<Graph['theme']>
+type ThemeKey = keyof UseGraphTheme
+
+type Theme<T extends ThemeKey> = {
+  themeProp: T,
+  value: UseGraphTheme[T],
+}
+
+export const useTheme = (graph: Graph, id: string) => {
+  const internalThemes: Theme<keyof UseGraphTheme>[] = []
+
+  const setTheme = <T extends keyof UseGraphTheme>(theme: Theme<T>) => {
+    removeTheme(theme.themeProp)
+    internalThemes.push(theme)
+  }
+
+  const removeTheme = (themeProp: ThemeKey) => {
+    internalThemes.filter(theme => theme.themeProp !== themeProp)
+  }
+
+  return {
+    setTheme,
+    removeTheme
+  }
+}
