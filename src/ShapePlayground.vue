c19f306fb9e8925624fcff013903592bb1391f7f
yonava <yona@vossandreae.com>
2024-10-31 00:36:32 -0400

    added shape playground component

--- src/ShapePlayground.vue
+++ src/ShapePlayground.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+import { useDark } from '@vueuse/core';
 import ResponsiveCanvas from '@utils/components/ResponsiveCanvas.vue';
 import colors from '@colors';
+
+const isDark = useDark();
+const color = computed(() => isDark.value ? colors.GRAY_800 : colors.GRAY_200);
+const patternColor = computed(() => (isDark.value ? colors.GRAY_200 : colors.GRAY_700) + '15');
 </script>
 
 <template>
-  <div>
+  <div class="h-full w-full">
     <ResponsiveCanvas
-      :color="colors."
+      :color="color"
+      :pattern-color="patternColor"
     ></ResponsiveCanvas>
   </div>
 </template>
