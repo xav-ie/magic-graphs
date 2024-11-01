2d361105ff668a18e2dd18f28929661937aecf2c
yonava <yona@vossandreae.com>
2024-11-01 02:17:57 -0400

    heatmap added to shape playground

--- src/ShapePlayground.vue
+++ src/ShapePlayground.vue
@@ -1,18 +1,30 @@
 <script setup lang="ts">
-  import { ref, computed } from "vue";
+  import { ref, computed, watch } from "vue";
   import { useDark } from "@vueuse/core";
   import ResponsiveCanvas from "@utils/components/ResponsiveCanvas.vue";
   import colors from "@colors";
-  import { shapes } from "@shapes"
+  import { shapes } from "@shapes";
+  import type { Shape } from "@shape/types";
+  import { debounce } from "@utils/debounce";
 
   const canvas = ref<HTMLCanvasElement>();
   const isDark = useDark();
+
   const color = computed(() =>
     isDark.value ? colors.GRAY_800 : colors.GRAY_200
   );
+
+  const heatmapActive = ref(false);
+  const heatmapBtnText = computed(() =>
+    heatmapActive.value ? "Hide Heatmap" : "Show Heatmap"
+  );
+  const heatmapResolution = ref(3);
+
   const patternColor = computed(
     () => (isDark.value ? colors.GRAY_200 : colors.GRAY_700) + "15"
   );
+
+  const items = ref<Shape[]>([]);
 
   const getCtx = () => {
     if (!canvas.value) throw new Error("canvas not found");
@@ -21,19 +33,51 @@
     return ctx;
   };
 
+  const testForHitbox = () => {
+    const { width, height } = canvas!.value!.getBoundingClientRect();
+    const SAMPLING_RATE = heatmapResolution.value;
+    for (let i = 0; i < width; i += SAMPLING_RATE) {
+      for (let j = 0; j < height; j += SAMPLING_RATE) {
+        const shapeHit = items.value.findLast((item) =>
+          item.hitbox({ x: i, y: j })
+        );
+        const textHit = items.value.findLast((item) =>
+          item.textHitbox?.({ x: i, y: j })
+        );
+        if (textHit) {
+          shapes
+            .circle({
+              at: { x: i, y: j },
+              radius: 2,
+              color: colors.YELLOW_500,
+            })
+            .draw(getCtx());
+        } else if (shapeHit) {
+          shapes
+            .circle({
+              at: { x: i, y: j },
+              radius: 2,
+              color: colors.RED_500,
+            })
+            .draw(getCtx());
+        } else {
+          shapes
+            .circle({
+              at: { x: i, y: j },
+              radius: 2,
+              color: colors.GREEN_500,
+            })
+            .draw(getCtx());
+        }
+      }
+    }
+  };
+
   const draw = () => {
     const ctx = getCtx();
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    const {
-      circle,
-      rect,
-      square,
-      triangle,
-      line,
-      arrow,
-      uturn,
-      cross,
-    } = shapes;
+    const { circle, rect, square, triangle, line, arrow, uturn, cross } =
+      shapes;
 
     const c = circle({
       at: { x: 200, y: 200 },
@@ -42,96 +86,45 @@
       stroke: {
         color: colors.BLUE_700,
         width: 10,
-      }
-    })
-    c.draw(ctx)
-
-    uturn({
-      color: colors.BLUE_500,
-      upDistance: 100,
-      downDistance: 100,
-      spacing: 15,
-      center: { x: 525, y: 100 },
-      lineWidth: 10,
-      angle: Math.PI / 4,
-      textArea: {
-        color: color.value,
-        text: {
-          content: "4",
-          color: colors.WHITE,
-        },
-      }
-    }).draw(ctx);
-
-    arrow({
-      start: { x: 300, y: 100 },
-      end: { x: 400, y: 200 },
-      color: colors.BLUE_600,
-      width: 10,
-    }).draw(ctx);
-
-    arrow({
-      start: { x: 400, y: 100 },
-      end: { x: 500, y: 200 },
-      color: colors.BLUE_600,
-      width: 10,
-      textArea: {
-        color: color.value,
-        text: {
-          content: "5",
-          color: colors.WHITE,
-        },
-      }
-    }).draw(ctx);
-
-
-    triangle({
-      point1: { x: 700, y: 300 },
-      point2: { x: 200, y: 500 },
-      point3: { x: 800, y: 500 },
-      color: colors.BLUE_600,
-    }).draw(ctx);
-
-    rect({
-      at: { x: 200, y: 600 },
-      width: 200,
-      height: 100,
-      color: colors.BLUE_700,
-    }).draw(ctx);
-
-    square({
-      at: { x: 500, y: 600 },
-      size: 100,
-      color: colors.BLUE_800,
-    }).draw(ctx);
-
-    line({
-      start: { x: 200, y: 800 },
-      end: { x: 800, y: 700 },
-      color: colors.BLUE_900,
-      width: 5,
-    }).draw(ctx);
-
-    cross({
-      center: { x: 800, y: 800 },
-      size: 100,
-      color: colors.GREEN_500,
-      lineWidth: 50,
-      angle: Math.PI / 3,
-    }).draw(ctx)
-
-    canvas.value?.addEventListener("click", (e) => {
-      const {offsetX: x, offsetY: y} = e
-      console.log(c.hitbox({x, y}));
+      },
     });
-  };
-  
 
+    const u = uturn({
+      center: { x: 400, y: 200 },
+      downDistance: 50,
+      upDistance: 50,
+      lineWidth: 10,
+      color: colors.BLUE_500,
+      angle: 0,
+      spacing: 20,
+      textArea: {
+        text: {
+          content: "Hello",
+          color: colors.BLUE_500,
+          fontSize: 20,
+        },
+      },
+    });
+
+    items.value.push(u);
+    items.value.push(c);
+
+    items.value.forEach((item) => {
+      item.draw(ctx);
+    });
+
+    if (heatmapActive.value) testForHitbox();
+  };
 
   document.addEventListener("resize", draw);
   document.addEventListener("keyup", (e) => {
     if (e.code === "Space") draw();
   });
+
+  const debouncedDraw = debounce(draw, 500);
+
+  watch(heatmapActive, draw);
+  watch(heatmapResolution, debouncedDraw);
 
   setTimeout(draw, 100);
 </script>
@@ -164,6 +157,49 @@
     >
       Redraw
     </button>
+    <button
+      @click="heatmapActive = !heatmapActive"
+      :class="[
+        'absolute',
+        'top-4',
+        'left-[105px]',
+        'px-2',
+        'py-1',
+
+        'bg-gray-800',
+        'text-gray-200',
+
+        'dark:bg-gray-200',
+        'dark:text-gray-800',
+
+        'hover:bg-gray-700',
+        'dark:hover:bg-gray-300',
+
+        'rounded-md',
+        'cursor-pointer',
+        'font-bold',
+        'z-50',
+      ]"
+    >
+      {{ heatmapBtnText }}
+    </button>
+    <div class="absolute top-4 left-[250px] flex items-center z-50 gap-4">
+      <input
+        v-if="heatmapActive"
+        type="range"
+        min="1"
+        max="10"
+        v-model.number="heatmapResolution"
+        id="heatmapResolution"
+      />
+      <label
+        class="text-white"
+        v-if="heatmapActive"
+        for="heatmapResolution"
+      >
+        Resolution: {{ heatmapResolution }}
+      </label>
+    </div>
     <ResponsiveCanvas
       @canvas-ref="(el) => (canvas = el)"
       :color="color"
