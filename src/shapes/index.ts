63fad20016e59914cb41ed53132c5fe90a1f0452
yonava <yona@vossandreae.com>
2024-10-31 20:55:19 -0400

    exposed all shape apis for magic shapes refactor

---
+++ src/shapes/index.ts
@@ -0,0 +1,17 @@
+import { arrow } from "./arrow";
+import { circle } from "./circle";
+import { line } from "./line";
+import { rect } from "./rect";
+import { square } from "./square";
+import { triangle } from "./triangle";
+import { uturn } from "./uturn";
+
+export const shapes = {
+  arrow,
+  circle,
+  line,
+  rect,
+  square,
+  triangle,
+  uturn,
+};
