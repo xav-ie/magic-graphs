1a21e0b21b5584aa6b36dd01618c0bfd74b78d27
yonava <yona@vossandreae.com>
2024-11-02 15:14:28 -0400

    added socket endpoints

--- server/src/sockets.ts
+++ server/src/sockets.ts
@@ -1,3 +1,45 @@
+import { Server } from 'socket.io'
 
+type GNode = {}
+type GEdge = {}
 
-console.log('Sockets!')
+interface GraphEvents {
+  nodeAdded: (node: GNode) => void
+  nodeRemoved: (node: GNode) => void
+  edgeAdded: (edge: GEdge) => void
+  edgeRemoved: (edge: GEdge) => void
+}
+
+const io = new Server<GraphEvents, GraphEvents, {}, {}>({
+  cors: {
+    origin: '*',
+  }
+})
+
+io.on('connection', (socket) => {
+  socket.on('nodeAdded', (node) => {
+    io.emit('nodeAdded', node)
+  })
+
+  socket.on('nodeRemoved', (node) => {
+    io.emit('nodeRemoved', node)
+  })
+
+  socket.on('edgeAdded', (edge) => {
+    io.emit('edgeAdded', edge)
+  })
+
+  socket.on('edgeRemoved', (edge) => {
+    io.emit('edgeRemoved', edge)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected')
+  })
+
+  socket.on('error', (error) => {
+    console.error(error)
+  })
+})
+
+console.log('Sockets Live!')
