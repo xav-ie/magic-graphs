4869cbdf42420894b0f84162ef3d199f28b640c5
yonava <yona@vossandreae.com>
2024-11-10 20:26:08 -0500

    added sim control type

---
+++ client/src/ui/sim/types.ts
@@ -0,0 +1,67 @@
+
+
+/**
+ * used as a standard for all simulation experiences across all products
+ */
+export type SimulationControls<T extends any[]> = {
+  /**
+   * skip forward to the next step.
+   * wont do anything if the current step is trace.length
+   */
+  nextStep: () => void
+  /**
+   * skip backward to the previous step.
+   * wont do anything if the current step is -1
+   */
+  prevStep: () => void
+
+  /**
+   * the current trace of the algorithm for which the simulation is being run
+   */
+  trace: Readonly<T>
+  /**
+   * the current step of the simulation.
+   * ranges from -1 to trace.length where -1 is the state before the algorithm has begun
+   * and trace.length is the state after the algorithm has completed
+   */
+  step: Readonly<number>
+  /**
+   * set the current step of the simulation
+   * @param step the step to set the simulation to
+   * @throws if step is not within the bounds of the trace (-1 to trace.length)
+   */
+  setStep: (step: number) => void
+
+  /**
+   * start the simulation. this will begin the simulation from step -1
+   */
+  start: () => void
+  /**
+   * stop the simulation. this will end the simulation and set the step to trace.length
+   */
+  stop: () => void
+  /**
+   * pause the simulation. keeps the playback interval running but stops the step from incrementing
+   */
+  paused: boolean
+  /**
+   * time, in milliseconds, between each step firing.
+   */
+  playbackSpeed: number
+
+  /**
+   * whether the simulation is currently active.
+   * changes to true when start is called and false when stop is called
+   */
+  isActive: Readonly<boolean>
+  /**
+   * whether the simulation is over.
+   * true when the step is trace.length
+   */
+  isOver: Readonly<boolean>
+  /**
+   * whether the simulation has begun.
+   * true when the step is greater than -1
+   */
+  hasBegun: Readonly<boolean>
+}
