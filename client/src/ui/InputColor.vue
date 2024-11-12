dc73fcb7fb221d20d5f7b609fad6ab27a3a0d0d1
yonava <yona@vossandreae.com>
2024-11-12 11:05:47 -0500

    added hex parsing to input

--- client/src/ui/InputColor.vue
+++ client/src/ui/InputColor.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { computed } from 'vue';
-import { isHexAlpha } from '@utils/colors';
+import { isHexAlpha, isHexStandard } from '@utils/colors';
 
 const model = defineModel<string>()
 
@@ -9,22 +9,44 @@ const colorValue = computed({
   set: (value: string) => model.value = value
 })
 
+/**
+ * isolates the alpha channel from the color (if it exists)
+ */
 const colorAlpha = computed(() => {
   if (!colorValue.value) return ''
-  if (isHexAlpha(colorValue.value)) return colorValue.value.slice(6)
+  if (isHexAlpha(colorValue.value)) return colorValue.value.slice(7)
   return ''
 })
 
+/**
+ * if we cannot get a color the html color input wants, we appease it with this
+ */
+const STAND_IN_COLOR = '#000000'
+
+/**
+ * returns a color that complies with the html color input
+ */
+const colorValueWithoutAlpha = computed(() => {
+  if (!colorValue.value) return STAND_IN_COLOR
+  else if (isHexAlpha(colorValue.value)) return colorValue.value.slice(0, 7)
+  else if (isHexStandard(colorValue.value)) return colorValue.value
+  return STAND_IN_COLOR
+})
+
+/**
+ * parses the color with the alpha channel back into the model
+ */
 const handleUpdate = (e: Event) => {
   if (!(e.target instanceof HTMLInputElement)) return
-  const inputtedColor = e.target.value
+  console.log('e.target.value', e.target.value, 'colorAlpha.value', colorAlpha.value)
+  colorValue.value = e.target.value + colorAlpha.value
 }
 </script>
 
 <template>
   <input
     @input="handleUpdate"
-    :value="colorValue"
+    :value="colorValueWithoutAlpha"
     type="color"
     :class="`p-1 w-10 h-full rounded-md appearance-none`"
   />
