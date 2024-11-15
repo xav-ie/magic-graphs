1540fbede9e7faa325d9682e91fdac3d22e5e4cf
yonava <yona@vossandreae.com>
2024-11-14 20:47:30 -0500

    converted flow graph to new edge creation rules

--- client/src/products/network-flow/settings.ts
+++ client/src/products/network-flow/settings.ts
@@ -7,6 +7,8 @@ export const FLOW_GRAPH_SETTINGS: Partial<GraphSettings> = {
   persistent: true,
   persistentStorageKey: 'network-flow',
   userAddedEdgeLabel: '5',
+  userAddedEdgeRuleNoSelfLoops: true,
+  userAddedEdgeRuleOneEdgePerPath: true,
   edgeInputToLabel: (input) => {
     const num = Number(input);
     const isValid = !isNaN(num) && num >= 0 && num < 100;
