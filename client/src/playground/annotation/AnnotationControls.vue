451f5d517a2daf422a21acc9007016f607f63196
Thomas Eykamp <embergameslp@gmail.com>
2024-11-17 14:34:59 -0500

    clean up UI

--- client/src/playground/annotation/AnnotationControls.vue
+++ client/src/playground/annotation/AnnotationControls.vue
@@ -14,43 +14,42 @@ const props = defineProps<{
 
 <template>
   <div
-    class="absolute bottom-12 flex w-full justify-center items-center gap-6 z-10"
+    class="absolute bottom-12 flex w-full justify-center z-10"
   >
-    <Button
-      v-for="color in brushColors"
-      :key="color"
-      @click="setColor(color)"
-      :color="color"
-      class="w-10 h-10"
-      :style="{ backgroundColor: color, borderRadius: '50%' }"
-    ></Button>
-  </div>
+    <div class="bg-gray-900 px-3 py-2 rounded-xl flex items-center gap-3">
+      <Button
+        v-for="color in brushColors"
+        :key="color"
+        @click="setColor(color)"
+        :color="color"
+        class="w-8 h-8"
+        :style="{ backgroundColor: color, borderRadius: '50%' }"
+      ></Button>
+      <div class="w-[1px] h-8 bg-gray-600"></div>
+      <Button
+        v-for="weight in brushWeights"
+        :key="weight"
+        @click="setBrushWeight(weight)"
+        :color="colors.TRANSPARENT"
+        class="h-[25px]"
+      >
+        <div
+          class="flex justify-center items-center bg-gray-300 rounded-md w-[15px]"
+          :style="{
+            height: `${weight * 2}px`,
+          }"
+        ></div>
+      </Button>
+      <div class="w-[1px] h-8 bg-gray-600"></div>
 
-  <div
-    class="absolute bottom-3 flex w-full justify-center items-center gap-2 z-10"
-  >
-    <Button
-      v-for="weight in brushWeights"
-      :key="weight"
-      @click="setBrushWeight(weight)"
-      :color="colors.TRANSPARENT"
-      class="h-[25px]"
-    >
-      <div
-        class="flex justify-center items-center bg-gray-200 rounded-md w-[42px]"
-        :style="{
-          height: `${weight * 2}px`,
-        }"
-      ></div>
-    </Button>
-  </div>
+      <btn @click="setEraser" class="text-white">
+        <v-icon>mdi-eraser </v-icon>
+      </btn>
+      <div class="w-[1px] h-8 bg-gray-600"></div>
 
-  <div class="flex flex-col gap-3 justify-center absolute h-full z-10 left-2">
-    <Button @click="setEraser">
-      <v-icon>mdi-eraser </v-icon>
-    </Button>
-    <Button @click="clear">
-      <v-icon>mdi-nuke</v-icon>
-    </Button>
+      <btn @click="clear" class="text-white">
+        <v-icon>mdi-nuke</v-icon>
+      </btn>
+    </div>
   </div>
 </template>
