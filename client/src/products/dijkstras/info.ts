67c56ffaa0dd530812b9829b6ce9524ac3a5f6f3
yonava <yona@vossandreae.com>
2024-11-18 13:51:11 -0500

    moved all pinfo for simulations to new system

--- client/src/products/dijkstras/info.ts
+++ client/src/products/dijkstras/info.ts
@@ -1,5 +1,19 @@
-import type { ProductInfo } from 'src/types'
+import type { ProductInfo, SimulationDeclaration } from 'src/types'
 import { useDijkstraSimulation } from './useSimulation'
+import type { Graph } from '@graph/types'
+
+const simulations = (graph: Graph): SimulationDeclaration[] => {
+  const controls = useDijkstraSimulation(graph)
+
+  return [
+    {
+      name: 'Dijkstras Algorithm',
+      description: 'Finds the shortest path from a source node to all other nodes in a graph',
+      thumbnail: '/products/thumbnails/dijkstras.png',
+      controls,
+    },
+  ]
+}
 
 const info: ProductInfo = {
   route: {
@@ -14,14 +28,7 @@ const info: ProductInfo = {
     description: 'Visualize Dijkstras Algorithm',
     thumbnail: '/products/thumbnails/dijkstras.png',
   },
-  simulations: (graph) => ([
-    {
-      name: 'Dijkstras Algorithm',
-      description: 'Finds the shortest path from a source node to all other nodes in a graph',
-      thumbnail: '/products/thumbnails/dijkstras.png',
-      controls: () => useDijkstraSimulation(graph),
-    },
-  ]),
+  simulations,
 }
 
 export default info
