15856152fd5d6bfaa4c05587b3f509bb70e68e1e
yonava <yona@vossandreae.com>
2024-11-18 16:51:13 -0500

    finished design for 404 page

--- client/src/router/404.vue
+++ client/src/router/404.vue
@@ -2,10 +2,14 @@
   <div
     class="w-full h-full dark:bg-gray-800 dark:text-white font-bold text-3xl flex justify-center"
   >
-    <div class="absolute text-center mt-12 max-w-[550px]">
-      <v-icon class="my-8" style="font-size: 150px">mdi-rabbit-variant-outline</v-icon>
+    <div class="absolute text-center mt-12 max-w-[600px] flex flex-col items-center justify-center gap-8">
+        <div class="bg-gray-900 rounded-full h-60 w-60 flex items-center justify-center">
+          <v-icon style="font-size: 150px">
+            mdi-rabbit-variant-outline
+          </v-icon>
+        </div>
       <h1>
-        Poof! Looks like
+        <span class="text-4xl">Poof!</span> <br> Looks like
         <span
           class="bg-gradient-to-tr dark:from-purple-600 dark:to-orange-500 from-purple-500 to-orange-500 text-transparent bg-clip-text"
         >
@@ -15,9 +19,7 @@
         <router-link
           to="/"
           class="text-blue-500 hover:underline"
-        >
-          return
-        </router-link>
+        >return</router-link>.
       </h1>
     </div>
   </div>
