9325a671be021e661c0284d0a94fd3098c21cc31
yonava <yona@vossandreae.com>
2024-11-24 20:32:04 -0800

    collab statics

---
+++ server/dist/public/assets/InputRange.vue_vue_type_script_setup_true_lang-B7iR_Hih.js
@@ -0,0 +1,1 @@
+import{e as l,G as n,K as s,L as u,o as m,c as r}from"./index-Bn4yAARZ.js";const i=l({__name:"InputRange",props:{modelValue:{},modelModifiers:{}},emits:["update:modelValue"],setup(t){const e=n(t,"modelValue");return(d,o)=>s((m(),r("input",{"onUpdate:modelValue":o[0]||(o[0]=a=>e.value=a),type:"range"},null,512)),[[u,e.value,void 0,{number:!0}]])}});export{i as _};
