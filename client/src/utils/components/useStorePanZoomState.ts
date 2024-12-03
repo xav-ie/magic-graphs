418feff99c735114b87ecbf7247594b6cc49f61d
yonava <yona@vossandreae.com>
2024-12-02 22:55:17 -0800

    pan zoom composition

---
+++ client/src/utils/components/useStorePanZoomState.ts
@@ -0,0 +1,56 @@
+import { onMounted, onUnmounted } from "vue";
+import type { Ref } from "vue";
+import { useLocalStorage } from "@vueuse/core";
+import { getCtx } from "@utils/ctx";
+
+export const useStorePanZoomState = (canvasRef: Ref<HTMLCanvasElement | undefined | null>) => {
+  const scale = useLocalStorage("panZoomScale", 0)
+  const panX = useLocalStorage("panZoomPanX", 0)
+  const panY = useLocalStorage("panZoomPanY", 0)
+
+  /**
+   * tracks the current pan and zoom of the canvas
+   */
+  const track = () => {
+    const ctx = getCtx(canvasRef);
+    const { a, e, f } = ctx.getTransform();
+    scale.value = a
+    panX.value = e
+    panY.value = f
+  }
+
+  /**
+   * applies the current pan and zoom to the canvas
+   */
+  const apply = () => {
+    const ctx = getCtx(canvasRef);
+    console.log("applying", scale.value, panX.value, panY.value)
+    ctx.resetTransform();
+    ctx.translate(panX.value, panY.value);
+    ctx.scale(scale.value, scale.value);
+  }
+
+  onMounted(() => {
+    if (!canvasRef.value) return;
+    apply();
+    canvasRef.value.addEventListener("wheel", track);
+    canvasRef.value.addEventListener("mousedown", track);
+    canvasRef.value.addEventListener("mouseup", track);
+  })
+
+  onUnmounted(() => {
+    if (!canvasRef.value) return;
+    canvasRef.value.removeEventListener("wheel", track);
+    canvasRef.value.removeEventListener("mousedown", track);
+    canvasRef.value.removeEventListener("mouseup", track);
+  })
+
+  return {
+    track,
+    apply,
+
+    scale,
+    panX,
+    panY,
+  }
+}
