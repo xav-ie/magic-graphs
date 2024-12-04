765557224cf2825551e18af51a6bc89bd93cd2de
yonava <yona@vossandreae.com>
2024-12-04 14:59:54 -0800

    converted product dropdown menu

--- client/src/ui/product/dropdown/ProductDropdown.vue
+++ client/src/ui/product/dropdown/ProductDropdown.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
-  import ProductDropdownButton from "./ProductDropdownButton.vue";
+  import CPopover from "@ui/core/Popover.vue";
   import ProductDropdownMenu from "./ProductDropdownMenu.vue";
 </script>
 
 <template>
-  <!-- <v-menu :offset="[10, 0]">
-    <template #activator="{ props }">
-      <ProductDropdownButton v-bind="props" />
+  <CPopover>
+    <template #activator="{ toggle }">
+      <button
+        @click="toggle"
+        class="bg-gray-800 hover:bg-gray-900 text-white px-4 py-2 text-xl font-bold rounded-lg"
+      >
+        <h1
+          class="bg-gradient-to-tr dark:from-purple-600 dark:to-orange-500 from-purple-500 to-orange-500 text-transparent bg-clip-text"
+        >
+          Magic Algorithms
+        </h1>
+      </button>
     </template>
 
     <ProductDropdownMenu />
-  </v-menu> -->
+  </CPopover>
 </template>
