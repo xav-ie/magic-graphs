14f13254bdc8a43273d6e39396415d4fc7562b49
Thomas Eykamp <embergameslp@gmail.com>
2024-12-08 01:59:30 -0500

    add dijkstras guard for no nodes

--- client/src/products/dijkstras/sim/guard.ts
+++ client/src/products/dijkstras/sim/guard.ts
@@ -6,5 +6,7 @@ export const canRunDijkstras = (graph: Graph) => () => {
   if (!isWeighted) return CANT_RUN_REASONS.NOT_WEIGHTED
   const nonNegativeWeights = graph.edges.value.every((e) => graph.helpers.getEdgeWeight(e.id) >= 0)
   if (!nonNegativeWeights) return CANT_RUN_REASONS.NEGATIVE_EDGE_WEIGHTS
+  const hasNodes = graph.nodes.value.length > 0
+  if (!hasNodes) return CANT_RUN_REASONS.NOT_ENOUGH_NODES(1)
   return true
 }
