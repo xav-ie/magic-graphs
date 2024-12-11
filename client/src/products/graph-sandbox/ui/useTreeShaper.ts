95504c33e42b584866ae5170b6dd4c38cb30b735
yonava <yona@vossandreae.com>
2024-12-11 14:44:22 -0800

    adding graph shaper

---
+++ client/src/products/graph-sandbox/ui/useTreeShaper.ts
@@ -0,0 +1,11 @@
+import type { Graph } from "@graph/types";
+
+export const useTreeShaper = (graph: Graph) => {
+  const shapeGraph = () => {
+    console.log('Shaping graph...')
+  }
+
+  return {
+    shapeGraph
+  }
+}
