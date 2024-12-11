7f225096799247010976261882158f88f886af76
yonava <yona@vossandreae.com>
2024-12-10 23:46:41 -0800

    updated statics

--- client/src/products/basic-search/settings.ts
+++ client/src/products/basic-search/settings.ts
@@ -4,5 +4,6 @@ import type { GraphSettings } from "@graph/settings";
  * settings for basic search useGraph instance
  */
 export const BASIC_SEARCH_GRAPH_SETTINGS: Partial<GraphSettings> = {
+  displayEdgeLabels: false,
   persistentStorageKey: "basic-search",
 }
