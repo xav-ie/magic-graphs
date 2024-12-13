ba0e325d075e91cce363b4f750f17827f716ec2b
yonava <yona@vossandreae.com>
2024-12-13 01:36:54 -0800

    added better styles to graph info on no nodes

--- client/src/products/graph-sandbox/ui/TreeShapeMenu.vue
+++ client/src/products/graph-sandbox/ui/TreeShapeMenu.vue
@@ -5,11 +5,11 @@
   import GraphNode from "@ui/graph/GNode.vue";
   import CPopover from "@ui/core/Popover.vue";
   import GWell from "@ui/graph/GWell.vue";
-  import CWell from "@ui/core/Well.vue";
-  import CIcon from "@ui/core/Icon.vue";
   import GButton from "@ui/graph/button/GButton.vue";
+  import CButton from "@ui/core/button/Button.vue";
   import colors from "@utils/colors";
   import type { GNode } from "@graph/types";
+  import TreeShapeMenuSettings from "./TreeShapeMenuSettings.vue";
 
   const props = defineProps<{
     controls: AutoTreeControls;
@@ -17,8 +17,13 @@
 
   const treeControls = toRef(props, "controls");
 
-  const { isActive, activate, deactivate, updateShape, rootNodeId } =
-    treeControls.value;
+  const {
+    isActive,
+    activate,
+    deactivate,
+    updateShape,
+    rootNodeId,
+  } = treeControls.value;
 
   const nodeSelected = (node: GNode) => {
     rootNodeId.value = node.id;
@@ -29,6 +34,13 @@
     if (!rootNodeId.value) return;
     return graph.value.getNode(rootNodeId.value);
   });
+
+  const turnOnAutoTree = () => {
+    activate();
+    if (!rootNode.value && graph.value.nodes.value.length > 0) {
+      rootNodeId.value = graph.value.nodes.value[0].id;
+    }
+  };
 </script>
 
 <template>
@@ -42,15 +54,10 @@
         v-if="graph.nodes.value.length > 0"
         class="flex flex-col gap-2"
       >
-        <h1 class="font-bold text-xl">Pick A Root Node</h1>
-
-        <CWell
-          v-if="isActive && rootNode"
-          :color="colors.RED_600"
-          class="rounded-md px-2 py-1 font-bold text-xs animate-pulse"
-        >
-          <h2>Actively Tracking Node {{ rootNode.label }}</h2>
-        </CWell>
+        <div class="flex justify-between">
+          <h1 class="font-bold text-xl">Pick A Root Node</h1>
+          <TreeShapeMenuSettings :controls="treeControls" />
+        </div>
 
         <GWell
           secondary
@@ -65,30 +72,33 @@
           />
         </GWell>
 
-        <div class="flex justify-between text-sm">
+        <div class="flex justify-between">
           <div>
-            <GButton
+            <CButton
               v-if="isActive"
               @click="deactivate"
-              contrast
+              :color="colors.RED_600"
+              class="text-xs"
             >
-              Disable Auto Format
-            </GButton>
+              Turn Off AutoTree™
+            </CButton>
             <GButton
               v-else
-              @click="activate"
+              @click="turnOnAutoTree"
               tertiary
+              class="text-xs"
             >
-              Enable Auto Format
+              Try AutoTree™
             </GButton>
           </div>
           <div>
-            <GButton tertiary>
-              <CIcon
-                class="text-sm"
-                icon="settings"
-              />
-            </GButton>
+            <GWell
+              secondary
+              v-if="isActive && rootNode"
+              class="rounded-md px-2 py-1 font-bold text-xs animate-pulse"
+            >
+              <h2>Tracking Node {{ rootNode.label }}</h2>
+            </GWell>
           </div>
         </div>
       </div>
