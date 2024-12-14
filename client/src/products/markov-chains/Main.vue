c6c32d79279aa1e977c8ddc8be3bc556c60c66ce
yonava <yona@vossandreae.com>
2024-12-13 16:15:40 -0800

    added markov product

---
+++ client/src/products/markov-chains/Main.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+  import { ref } from "vue";
+  import { useGraph } from "@graph/useGraph";
+  import GraphProduct from "@ui/product/GraphProduct.vue";
+  import { MARKOV_CHAIN_GRAPH_SETTINGS } from "./settings";
+
+  const graphEl = ref<HTMLCanvasElement>();
+  const graph = useGraph(graphEl, MARKOV_CHAIN_GRAPH_SETTINGS);
+</script>
+
+<template>
+  <GraphProduct
+    @graph-ref="(el) => (graphEl = el)"
+    :graph="graph"
+  >
+    <template #top-center>
+
+    </template>
+
+    <template #center-left>
+
+    </template>
+  </GraphProduct>
+</template>
