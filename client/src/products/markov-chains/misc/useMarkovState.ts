05d14ffe28a98dad6304ca1bd7a9f9f1119f1ad8
yonava <yona@vossandreae.com>
2024-12-13 17:35:24 -0800

    created markov colorizer

--- client/src/products/markov-chains/misc/useMarkovState.ts
+++ client/src/products/markov-chains/misc/useMarkovState.ts
@@ -1,13 +1,30 @@
+import { computed } from "vue";
 import type { Graph } from "@graph/types";
 import { useComponentAdjacencyMap } from "./useComponentAdjacencyMap";
 import { useMarkovClasses } from "./useMarkovClasses";
+
+/**
+ * reduce an array of sets into a single set
+ *
+ * @param sets array of sets
+ * @returns a single set containing all elements from the input sets
+ * @example reduceSet([new Set([1, 2]), new Set([2, 3])]) // Set(1, 2, 3)
+ */
+export const reduceSet = <T>(sets: Set<T>[]) => {
+  return sets.reduce((acc, set) => new Set([...acc, ...set]), new Set<T>())
+}
 
 export const useMarkovState = (graph: Graph) => {
   const componentMap = useComponentAdjacencyMap(graph);
   const classes = useMarkovClasses(graph, componentMap);
 
+  const recurrentStates = computed(() => reduceSet(classes.value.recurrent));
+  const transientStates = computed(() => reduceSet(classes.value.transient));
+
   return {
     componentMap,
-    classes
+    classes,
+    recurrentStates,
+    transientStates,
   }
 }
---
+++ client/src/products/markov-chains/ui/useMarkovColorizer.ts
@@ -0,0 +1,36 @@
+import { useTheme } from "@graph/themes/useTheme";
+import type { GNode, Graph } from "@graph/types";
+import { useSCCColorizer } from "@product/graph-sandbox/ui/GraphInfoMenu/useSCCColorizer";
+import { TRANSIENT_COLOR } from "./colors";
+import { useMarkovState } from "../misc/useMarkovState";
+
+export const USETHEME_ID = 'markov-chains';
+
+export const useMarkovColorizer = (graph: Graph) => {
+  const markovState = useMarkovState(graph);
+  const sccColorizer = useSCCColorizer(graph);
+
+  const { setTheme, removeTheme } = useTheme(graph, USETHEME_ID);
+
+  const colorNodeBorder = (node: GNode) => {
+    if (graph.focus.isFocused(node.id)) return
+    if (markovState.recurrentStates.value.has(node.id)) return TRANSIENT_COLOR
+  }
+
+  const colorize = () => {
+    sccColorizer.colorize();
+    setTheme('nodeBorderColor', colorNodeBorder);
+    setTheme('nodeAnchorColor', colorNodeBorder);
+  }
+
+  const decolorize = () => {
+    sccColorizer.decolorize();
+    removeTheme('nodeBorderColor');
+    removeTheme('nodeAnchorColor');
+  }
+
+  return {
+    colorize,
+    decolorize,
+  }
+}
