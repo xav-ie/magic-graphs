345b044f2df3a53b1366732214e1f5ef60213013
yonava <yona@vossandreae.com>
2024-12-15 01:54:16 -0800

    added docs and infinite trace support to sim controls

--- client/src/ui/product/sim/useSimulationControls.ts
+++ client/src/ui/product/sim/useSimulationControls.ts
@@ -1,7 +1,7 @@
 import { computed, ref, watch } from "vue";
-import type { ComputedRef, Ref } from "vue";
+import type { Ref } from "vue";
 import { graph } from "@graph/global";
-import type { SimulationControls } from "@ui/product/sim/types";
+import type { SimulationControls, SimulationTrace } from "@ui/product/sim/types";
 
 type SimulationControlsOptions = {
   /**
@@ -25,8 +25,8 @@ const DEFAULT_OPTIONS = {
  */
 export const DEFAULT_PLAYBACK_SPEED = 1000;
 
-export const useSimulationControls = <T extends any[]>(
-  trace: ComputedRef<T>,
+export const useSimulationControls = <T>(
+  trace: SimulationTrace<T>,
   options: SimulationControlsOptions = {}
 ): SimulationControls<T> => {
   const { allowEditingDuringPlayback, lastStep } = {
@@ -34,15 +34,53 @@ export const useSimulationControls = <T extends any[]>(
     ...options,
   };
 
-  const simLastStep = computed(() => lastStep?.value ?? trace.value.length);
+  /**
+   * the last step of the simulation
+   */
+  const simLastStep = computed(() => {
+    if (lastStep) return lastStep.value; // handles custom
+    if (Array.isArray(trace)) return trace.length; // handles TraceArray<T>
+    return Infinity; // handles TraceFunction<T>
+  });
+
+  /**
+   * the current step of the simulation
+   */
   const step = ref(0);
+
+  /**
+   * whether the simulation is paused
+   */
   const paused = ref(true);
+
+  /**
+   * the playback speed in ms per step of the simulation
+   */
   const playbackSpeed = ref(DEFAULT_PLAYBACK_SPEED);
+
+  /**
+   * whether the simulation is actively being played back (even if paused)
+   */
   const active = ref(false);
+
+  /**
+   * the interval that advances the simulation
+   */
   const interval = ref<NodeJS.Timeout | undefined>();
+
+  /**
+   * whether the simulation has reached the last step
+   */
   const isOver = computed(() => step.value === simLastStep.value);
+
+  /**
+   * whether the simulation is past the initial step
+   */
   const hasBegun = computed(() => step.value > 0);
 
+  /**
+   * kicks off the simulation playback
+   */
   const start = () => {
     if (active.value) return;
 
@@ -55,34 +93,51 @@ export const useSimulationControls = <T extends any[]>(
     setupPlaybackInterval();
   };
 
+  /**
+   * shuts down the simulation playback
+   */
   const stop = () => {
     if (interval.value) clearInterval(interval.value);
     graph.value.settings.value.interactive = true;
     active.value = false;
   };
 
+  /**
+   * sets up the interval that advances the simulation
+   */
   const setupPlaybackInterval = () => {
     if (interval.value) clearInterval(interval.value);
+    // this condition should never be true, but
+    // this loop should never be executing if the playback is not active
+    if (!active.value) return;
     interval.value = setInterval(() => {
       if (isOver.value || paused.value) return;
       nextStep();
     }, playbackSpeed.value);
   };
 
-  watch(playbackSpeed, () => {
-    if (active.value) setupPlaybackInterval();
-  });
+  // makes the interval reactive to changes in the playback speed
+  watch(playbackSpeed, setupPlaybackInterval);
 
+  /**
+   * advances the simulation to the next step
+   */
   const nextStep = () => {
     if (isOver.value) return;
     step.value++;
   };
 
+  /**
+   * rewinds the simulation to the previous step
+   */
   const prevStep = () => {
     if (!hasBegun.value) return;
     step.value--;
   };
 
+  /**
+   * sets the simulation to a specific step
+   */
   const setStep = (newStep: number) => {
     if (newStep < 0 || newStep > simLastStep.value) return;
     step.value = newStep;
