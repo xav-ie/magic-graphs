883be85bb4b3636f0872dc311465e16063faa5c9
yonava <yona@vossandreae.com>
2024-12-15 00:31:58 -0800

    added sets utils

--- client/src/products/markov-chains/misc/useMarkovChain.ts
+++ client/src/products/markov-chains/misc/useMarkovChain.ts
@@ -1,21 +1,11 @@
 import { computed } from "vue";
-import type { GNode, Graph } from "@graph/types";
+import type { Graph } from "@graph/types";
+import { reduceSet } from "@utils/sets";
 import { useComponentAdjacencyMap } from "./useComponentAdjacencyMap";
 import { useMarkovClasses } from "./useMarkovClasses";
 import { useMarkovPeriodicity } from "./useMarkovPeriodicity";
 import { useMarkovSteadyState } from "./useMarkovSteadyState";
 import { useMarkovNodeWeights } from "./useMarkovNodeWeights";
-
-/**
- * reduce an array of sets into a single set
- *
- * @param sets array of sets
- * @returns a single set containing all elements from the input sets
- * @example reduceSet([new Set([1, 2]), new Set([2, 3])]) // Set(1, 2, 3)
- */
-export const reduceSet = <T>(sets: Set<T>[]) => {
-  return sets.reduce((acc, set) => new Set([...acc, ...set]), new Set<T>())
-}
 
 /**
  * reactive markov chain characteristics
