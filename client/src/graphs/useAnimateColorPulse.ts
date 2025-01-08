1fe13bf729591eac0513d7d4c7c0fa2f1bb0f819
eykamt <embergameslp@gmail.com>
2025-01-07 20:53:20 -0500

    working reactive list

--- client/src/graphs/useAnimateColorPulse.ts
+++ client/src/graphs/useAnimateColorPulse.ts
@@ -6,32 +6,44 @@ import { interpolateColors, EASING_FUNCTIONS } from "@utils/animate";
 export const useAnimateColorPulse = (graph: Graph) => {
   const { setTheme, removeAllThemes } = useTheme(graph, "pulse_animation");
 
-  const edgesToPulseColor = ref<Set<GEdge['id']>>(new Set());
+  const pulsingEdges = ref<Set<GEdge["id"]>>(new Set());
   const animateInterval = ref<NodeJS.Timeout | null>(null);
   const pulseColor = ref("#FFFFFF");
-  
+
   const easingFunction = EASING_FUNCTIONS["in-out"];
-  const PULSE_DURATION = 1000; 
-  
+  const pulseDuration = ref(1000);
+
   const clearAll = () => {
-    edgesToPulseColor.value.clear();
-    removeAllThemes();  
+    pulsingEdges.value.clear();
+    removeAllThemes();
     if (animateInterval.value !== null) {
       clearInterval(animateInterval.value);
     }
   };
-  
+
   const animate = () => {
     const baseColor = graph.baseTheme.value.edgeColor;
     const currentTime = Date.now();
-    const elapsed = currentTime % PULSE_DURATION;
-    const progress = elapsed / PULSE_DURATION;
-    const easedProgress = easingFunction(progress < 0.5 ? progress : 1 - progress); // oscillate
-    const interpolatedColor = interpolateColors([baseColor, pulseColor.value], easedProgress);
-    setTheme("edgeColor", interpolatedColor.toHexString());
-  };
+    const elapsed = currentTime % pulseDuration.value;
+    const progress = elapsed / pulseDuration.value;
+    const easedProgress = easingFunction(
+      progress < 0.5 ? progress : 1 - progress
+    ); // oscillate
+    const interpolatedColor = interpolateColors(
+      [baseColor, pulseColor.value],
+      easedProgress
+    );
 
-  animateInterval.value = setInterval(animate, 25)
+    const colorEdge = (edge: GEdge) => {
+      return pulsingEdges.value.has(edge.id)
+        ? interpolatedColor.toHexString()
+        : graph.baseTheme.value.edgeColor;
+    };
+
+    setTheme("edgeColor", colorEdge);
+  };
+  
+  animateInterval.value = setInterval(animate, 25);
 
   onUnmounted(() => {
     clearAll();
@@ -40,7 +52,8 @@ export const useAnimateColorPulse = (graph: Graph) => {
   return {
     clearAll,
 
-    edgesToPulseColor,
+    pulsingEdges,
     pulseColor,
+    pulseDuration,
   };
 };
