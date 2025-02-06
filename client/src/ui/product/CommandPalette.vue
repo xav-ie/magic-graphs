d89e40c304a71048e3dd84e0f0d64a125061cdea
Thomas Eykamp <embergameslp@gmail.com>
2025-02-06 16:05:55 -0500

    add help button

---
+++ client/src/ui/product/CommandPalette.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import GButton from "@ui/graph/button/GButton.vue";
+import CIcon from "@ui/core/Icon.vue";
+</script>
+
+<template>
+  <GButton>
+    <CIcon icon="question_mark"></CIcon>
+  </GButton>
+</template>
