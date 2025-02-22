cee5647aacc1d5ad65776c897b346a2088fd0ecb
eykamt <embergameslp@gmail.com>
2025-02-22 00:00:46 -0500

    cleanup code to address some review comments

--- client/src/utils/components/useCanvasCamera.ts
+++ client/src/utils/components/useCanvasCamera.ts
@@ -1,9 +1,15 @@
-import { ref, onMounted, onUnmounted } from "vue";
+import { ref, onMounted, onUnmounted, computed } from "vue";
 import type { Coordinate } from "@shape/types";
+import { MOUSE_BUTTONS } from "@graph/global";
 
+/**
+ * Handles panning canvas by scrolling the overflow of the canvas container, called by the 'responsive-canvas-container' id.
+ *
+ * @returns variables to check if panning is allowed or happening and a function to teleport camera
+ *
+ */
 export const useCanvasCamera = () => {
   const allowPanning = ref(true);
-
   const isPanning = ref(false);
   const panStart = ref<Coordinate>({ x: 0, y: 0 });
   const scrollStart = ref<Coordinate>({ x: 0, y: 0 });
@@ -12,8 +18,7 @@ export const useCanvasCamera = () => {
   const startPan = (event: MouseEvent) => {
     if (!allowPanning.value) return;
     if (!parentEl.value) return;
-
-    if (event.button !== 2) return;
+    if (event.button !== MOUSE_BUTTONS.middle) return;
 
     event.preventDefault();
     isPanning.value = true;
@@ -41,42 +46,42 @@ export const useCanvasCamera = () => {
    * @param options
    */
   const teleportTo = (options: ScrollToOptions) => {
-    if (parentEl.value) {
-      const { left = 0, top = 0, behavior } = options;
-      const x = left - window.screen.width / 2;
-      const y = top - window.screen.height / 2;
-      parentEl.value.scrollTo({ left: x, top: y, behavior });
-    }
+    if (!parentEl.value) return;
+
+    const { left = 0, top = 0, behavior } = options;
+    const x = left - window.screen.width / 2;
+    const y = top - window.screen.height / 2;
+    parentEl.value.scrollTo({ left: x, top: y, behavior });
   };
 
   onMounted(() => {
     parentEl.value = document.getElementById("responsive-canvas-container");
-    if (parentEl.value) {
-      parentEl.value.addEventListener("mousedown", startPan);
-      parentEl.value.addEventListener("mousemove", pan);
-      parentEl.value.addEventListener("mouseup", stopPan);
-      parentEl.value.addEventListener("mouseleave", stopPan);
-      parentEl.value.addEventListener("contextmenu", (event) =>
-        event.preventDefault(),
-      );
-    }
+    if (!parentEl.value) return;
+
+    parentEl.value.addEventListener("mousedown", startPan);
+    parentEl.value.addEventListener("mousemove", pan);
+    parentEl.value.addEventListener("mouseup", stopPan);
+    parentEl.value.addEventListener("mouseleave", stopPan);
+    parentEl.value.addEventListener("contextmenu", (event) =>
+      event.preventDefault(),
+    );
   });
 
   onUnmounted(() => {
-    if (parentEl.value) {
-      parentEl.value.removeEventListener("mousedown", startPan);
-      parentEl.value.removeEventListener("mousemove", pan);
-      parentEl.value.removeEventListener("mouseup", stopPan);
-      parentEl.value.removeEventListener("mouseleave", stopPan);
-      parentEl.value.removeEventListener("contextmenu", (event) =>
-        event.preventDefault(),
-      );
-    }
+    if (!parentEl.value) return;
+
+    parentEl.value.removeEventListener("mousedown", startPan);
+    parentEl.value.removeEventListener("mousemove", pan);
+    parentEl.value.removeEventListener("mouseup", stopPan);
+    parentEl.value.removeEventListener("mouseleave", stopPan);
+    parentEl.value.removeEventListener("contextmenu", (event) =>
+      event.preventDefault(),
+    );
   });
 
   return {
     allowPanning,
-    isPanning,
+    isPanning: computed(() => isPanning.value),
 
     teleportTo,
   };
