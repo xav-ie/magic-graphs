f7f15569ed1508d0cfa121645b104b04bdde042d
eykamt <embergameslp@gmail.com>
2025-03-05 22:34:02 -0500

    add zooming to center screen

--- client/src/utils/components/usePinchToZoom.ts
+++ client/src/utils/components/usePinchToZoom.ts
@@ -10,13 +10,17 @@ export function usePinchToZoom(
   canvasRef: Ref<HTMLCanvasElement | undefined | null>,
 ) {
   const zoomOrigin = ref({ x: 0, y: 0 });
+  let isScrolling = false;
 
   const handleWheel = (ev: WheelEvent) => {
     if (!ev.ctrlKey) return;
     ev.preventDefault();
+    isScrolling = true;
+
     const canvas = canvasRef.value;
     if (!canvas) return;
     const rect = canvas.getBoundingClientRect();
+
     const cursorX = ev.clientX - rect.left;
     const cursorY = ev.clientY - rect.top;
 
@@ -50,9 +54,29 @@ export function usePinchToZoom(
     canvas.removeEventListener('wheel', handleWheel);
   });
 
-  watch(scale, () => {
-    scale.value = Math.min(MAX_SCALE, Math.max(MIN_SCALE, scale.value));
+  watch(scale, (newScale, oldScale) => {
+    scale.value = Math.min(MAX_SCALE, Math.max(MIN_SCALE, newScale));
+
+    const canvas = canvasRef.value;
+    if (!canvas) return;
+
+    if (!isScrolling) {
+      const rect = canvas.getBoundingClientRect();
+      const screenCenterX = window.innerWidth / 2;
+      const screenCenterY = window.innerHeight / 2;
+
+      const canvasX = screenCenterX - rect.left;
+      const canvasY = screenCenterY - rect.top;
+
+      zoomOrigin.value.x =
+        canvasX - (canvasX - zoomOrigin.value.x) * (scale.value / oldScale);
+      zoomOrigin.value.y =
+        canvasY - (canvasY - zoomOrigin.value.y) * (scale.value / oldScale);
+    }
+
     applyZoom();
+
+    isScrolling = false;
   });
 
   return {
