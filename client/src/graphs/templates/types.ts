e0f32420fce575837f1b7a0d563c0f297b5b541a
teykamp <thomas@eykamp.com>
2025-03-16 20:18:31 -0400

    add jsdoc

--- client/src/graphs/templates/types.ts
+++ client/src/graphs/templates/types.ts
@@ -1,5 +1,16 @@
 import type { GEdge, GNode } from '@graph/types';
 
+/**
+ * @description
+ * GraphTemplate is a type that represents a template for a graph.
+ * It contains the following properties:
+ * - id: the name for the template
+ * - isUserAdded: whether this is a default template or a user-added template
+ * - thumbnail: string that can be used to display a thumbnail of the graph
+ * - title: text displayed along with the template in the UI
+ * - description: text displayed along with the template in the UI
+ * - graphState: the state of the graph
+ */
 export type GraphTemplate = {
   id: string;
   isUserAdded?: boolean;
